6ba4380910f9bba392e020e76a7b9a79d4f881cd
melissa-k <34781075+Melissa-k@users.noreply.github.com>
2018-03-21 10:54:25 +0100

    les fonctions de calculs de performances sur le {carré} imputé par le reseau de neurone profond

--- src/modelutil.py
+++ src/modelutil.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 23 15:35:26 2018
-
 @author: jbrlod
+modified by A.Rimoux & M.Kouassi
 """
 
 from keras import losses
@@ -12,6 +12,8 @@ from keras.models import Model
 from keras.layers.convolutional import Conv2DTranspose,Conv2D
 from keras.layers.core import Activation
 from keras.layers import MaxPooling2D, concatenate, Input
+from sklearn.metrics import mean_squared_error as rmse1
+import numpy as np
 
 def get_model_4layers(img_rows,img_cols):
     #mettre les inputs
@@ -140,6 +142,106 @@ def masked_mse(y_true,y_pred):
     isMask = 1 - K.cast(isMask,dtype=K.floatx())
     y_true = y_true*isMask
     y_pred = y_pred*isMask
+    return losses.mean_squared_error(y_true,y_pred)
+
+def mask_apply(y_true, y_pred):
+    """ Application du masque pour travailler uniquement sur 
+    le carré imputé par le réseau de neurone profond.
+    y_true_m et y_pred_m sont uniquement le carré ou la forme imputée 
+    par le reseau de neurone profond. 
+    y_true et y_pred sont le carré ou la forme imputée 
+    par le reseau de neurone profond , entouré par zero pour le reste de l'imagette."""
+    nanval = -1e5
+    # Réduction de dimension un array 2d (64*64)
+    y_true.shape ; y_true = np.squeeze(y_true)  
+    y_pred.shape; y_pred = np.squeeze(y_pred)
+    # Définition et binarisation du masque
+    isMask = np.empty_like(y_true)
+    isMask[y_true == nanval] = 0
+    isMask[y_true != nanval] = 1
+    # Application du masque sur les y_true et y_pred
+    y_true = y_true * isMask;
+    y_pred = y_pred * isMask;
+    y_true_m = y_true[y_true != 0]
+    y_pred_m = y_pred[y_true != 0]
+    return y_true, y_pred, y_true_m, y_pred_m
+
+def mask_apply_crop(y_true, y_pred, cwidth, cheight, cb):
+    """ Cette fonction permet de selectionner les ytrue et ypred qu'on 
+    veut selon la position dans l'image du carré dans l'image.
+    cheight : est la hauteur à prendre ou pas  en compte.
+    cwidth :  est la largeur à prendre ou pas en compte.
+    cb : booléen : if [True : crop extérieur] et [False : crop intérieur]"""
+    nanval = -1e5
+    # Définition et binarisation du masque
+    isMask = np.empty_like(y_true)
+    isMask[y_true == nanval] = 0
+    isMask[y_true != nanval] = 1
+    isMask = np.squeeze(isMask) 
+    # Reduction de dimension de (64*64*1) à (64*64)
+    isMask = np.squeeze(isMask) 
+    y_true = np.squeeze(y_true)
+    y_pred = np.squeeze(y_pred)
+    # Mask de selection des y true à plotter
+    if (cb == True):
+        bigmask = np.zeros(shape=(64,64))
+        bigmask[cwidth:-cwidth, cheight:-cheight] = 1  
+        isMask1 = isMask * bigmask
+    elif (cb == False):
+        bigmask = np.ones(shape=(64,64))
+        bigmask[cwidth:-cwidth, cheight:-cheight] = 0  
+        isMask1 = isMask * bigmask   
+    # Application du masque sur 
+    y_true = y_true * isMask1
+    y_pred = y_pred * isMask1
+    y_true_m = y_true[y_true != 0]
+    y_pred_m = y_pred[y_true != 0]
+    return y_true, y_pred, y_true_m, y_pred_m
+
+def test_masked_mse(y_true,y_pred):
+    """calcul du loss par root mean square (sklearn)"""
+    ytr, yp, y_tr_m, y_pr_m = mask_apply(y_true,y_pred)
+    mseLoss = rmse1(ytr,yp)
+    return mseLoss
+
+def test_masked_corrcoef(y_true,y_pred):
+    """calcul du coefficient de correlation entre chla predict et chla true 
+    du carré imputé par le reseau de neurones profond. """
+    ytr, yp, y_tr_m, y_pr_m = mask_apply(y_true,y_pred)
+    CorrCoef = np.corrcoef(y_tr_m, y_pr_m, bias=True)[0][1]
+    return CorrCoef
+
+def test_pixel_masked_loss(y_true, y_pred):
+    ytrue, ypred, ytrue_m, ypred_m = mask_apply(y_true,y_pred)
+    i = np.where(ytrue != 0)
+    index_dim = np.shape(i)
+    i_center = int(index_dim[1]/2) # indice de l'indice central du mask
+    b = np.subtract(ytrue, ypred)
+    # pixel central
+    ix_center = i[0][i_center]
+    iy_center = i[1][i_center]
+    chla_center = b[ix_center,iy_center]
+
+    # pixel central supérieur
+    ix_up = i[0][0]
+    chla_upcenter = b[ix_up,iy_center]
+
+    # pixel central inférieur
+    ix_down = i[0][-1]
+    chla_DC = b[ix_down,iy_center]
+
+    # pixel central gauche
+    iy_left = i[1][0]
+    chla_LC = b[ix_center,iy_left]
+
+    # pixel central droit
+    iy_right = i[1][-1]
+    chla_RC = b[ix_center,iy_right]
+
+    # pixel  des coins 
+    chla_UL = b[ix_up,iy_left]       # coin supérieur gauche
+    chla_UR = b[ix_up,iy_right]      # coin supérieur droit
+    chla_DL = b[ix_down, iy_left]    # coin inférieur gauche
+    chla_DR = b[ix_down, iy_right]   # coin inférieur droit 
     
-    return (losses.mean_squared_error(y_true,y_pred))
-    
+    return chla_center, chla_UL, chla_UR, chla_DL, chla_DR
